94cc35ed01282853688d02c575a01444d6b3d785
n0f3 <n0f3@outlook.com>
2020-10-02 18:15:22 -0400

    Completed reverse number

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// My solution
+function reverseInt(n) {
+  const returnNumber = [];
+  const numberString = n.toString();
+  if (numberString[0] === '-') {
+    returnNumber[0] = '-'
+  } else {
+    returnNumber[0] = '0'
+  }
+  for(let i = numberString.length - 1; i >= 0; i--) {
+    if (numberString[i] === '0') continue;
+    returnNumber.push(numberString[i]);
+  }
+  return parseInt(returnNumber.join(''), 10);
+}
+
+// More solutions
+// Could use Math.sign which return 1 for positive or -1 for negative
+// function reverseIn(n) {
+//   const reversed = n.toString().split('').reverse().join('');
+//   return parseInt(reversed) * Math.sign(n);
+// }
 
 module.exports = reverseInt;
