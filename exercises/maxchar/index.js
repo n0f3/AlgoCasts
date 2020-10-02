0e87c3b4607a5215049ab028d0d7926b344f8b6a
n0f3 <n0f3@outlook.com>
2020-10-02 18:47:10 -0400

    Completed maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const occurrencies = {};
+  for(const char of str) {
+    occurrencies[char] = occurrencies[char] ? occurrencies[char] + 1 : 1;
+  }
+  let max = -Infinity;
+  let maxKey;
+  for(const prop in occurrencies) {
+    if (occurrencies[prop] > max) {
+      max = occurrencies[prop];
+      maxKey = prop;
+    }
+  }
+  return maxKey;
+}
 
 module.exports = maxChar;
