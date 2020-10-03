b958266e8f1c12bd5d876b6ee390ec44ab0383f9
n0f3 <n0f3@outlook.com>
2020-10-02 20:45:54 -0400

    Completed chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,44 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// My solution
+function chunk(array, size) {
+  let start = 0;
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if ((i + 1) % size === 0 ) {
+      result.push(array.slice(start, i + 1));
+      start = i + 1
+    }
+  }
+  if (start < array.length)
+    result.push(array.slice(start));
+  return result;
+}
+
+// Other solutions
+// Better than mine, it doesn't need to slice anything
+// function chunk(array, size) {
+//   const chunked = [];
+//   for(const element of array) {
+//     const last = chunked[chunked.length - 1];
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else last.push(element);
+//   }
+//   return chunked;
+// }
+
+// Similar to mine, I didn't have to check for where I was in the array
+// I could just increment the index by size every time
+// function chunk(array, size) {
+//   const chunked = [];
+//   let index = 0;
+//   while (index < array.length) {
+//     chunked.push(array.slice(index, index + size));
+//     index += size;
+//   }
+//   return chunked;
+// }
 
 module.exports = chunk;
