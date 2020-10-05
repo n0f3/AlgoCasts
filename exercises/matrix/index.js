3e6e94df482710b744ef38fd625168d3f014709e
n0f3 <n0f3@outlook.com>
2020-10-05 14:51:12 -0400

    Finished spiral matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,36 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let counter = 1;
+  let results = [];
+  for(let j = 0; j < n; j++) {
+    results.push([]);
+  }
+  let startColumn = 0, endColumn = n - 1, startRow = 0, endRow = n - 1;
+  while (startColumn <= endColumn && startRow <= endRow) {
+    for(let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+    for(let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+    for(let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+    for(let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+  return results;
+}
 
 module.exports = matrix;
